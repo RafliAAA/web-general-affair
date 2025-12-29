edbfe7ef78b8a89f0dc8e7c82c12f3a6b02fd80e
rafliAAA <raflialamsyah184@gmail.com>
2025-12-29 21:58:06 +0700

    feat: add auth routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,4 +5,10 @@ dotenv.config();
 
 const app = express();
 
+app.use(express.json());
+
+import authRoutes from "./modules/auth/auth.routes";   
+
+app.use("/api/auth", authRoutes);
+
 export default app
