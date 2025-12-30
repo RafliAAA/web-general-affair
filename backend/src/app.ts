cafe67fbfd69f592fb5afde60b383fff89d9f3bb
rafliAAA <raflialamsyah184@gmail.com>
2025-12-30 19:49:32 +0700

    feat: add auth routes

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,15 @@
 import express  from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./modules/auth/auth.routes";   
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
-
-import authRoutes from "./modules/auth/auth.routes";   
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
