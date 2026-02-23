6047fac1610e274433b5a2e50d79d51f54a8abc1
rafliAAA <raflialamsyah184@gmail.com>
2026-02-24 02:56:25 +0700

    add auth types

---
+++ frontend/src/types/auth.tsx
@@ -0,0 +1,36 @@
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  role?: string; // optional, kalau ada role
+}
+
+export interface AuthState {
+  user: User | null;
+  loading: boolean;
+  checkingAuth: boolean;
+  signup: (data: SignupPayload) => Promise<void>;
+  login: (email: string, password: string) => Promise<void>;
+  logout: () => Promise<void>;
+  checkAuth: () => Promise<void>;
+}
+
+export interface SignupPayload {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export interface LoginPayload {
+  email: string;
+  password: string;
+}
+
+export interface ApiError {
+  response?: {
+    data?: {
+      message?: string;
+    };
+  };
+}
