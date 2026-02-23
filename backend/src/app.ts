8e4bac9b7e70ffa15eeba5be46d761cd3494bcf0
rafliAAA <raflialamsyah184@gmail.com>
2026-02-24 02:55:32 +0700

    add cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 import authRoutes from "./modules/auth/auth.routes";
 import assetsRoutes from "./modules/assets/assets.routes";
@@ -11,6 +12,12 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/assets", assetsRoutes);
