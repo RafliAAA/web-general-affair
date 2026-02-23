4ef991c919cbfa1ccf73a9e3da703d34a6aa410f
rafliAAA <raflialamsyah184@gmail.com>
2026-02-24 03:04:29 +0700

    add zustand n sonner

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,49 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Dashboard from "../src/pages/Dashboard";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { lazy, Suspense, useEffect } from "react";
+import { Toaster } from "sonner";
+import Spinner from "./components/layout/Spinner";
+import { useAuthStore } from "./features/auth/stores/useAuthStore";
+
+const Dashboard = lazy(() => import("./pages/Dashboard"));
+const Projects = lazy(() => import("./pages/projects/Projects"));
+const LoansPage = lazy(() => import("./pages/Loans"));
+const ReturnsPage = lazy(() => import("./pages/Returns"));
+const Assets = lazy(() => import("./features/assets/Assets"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Login = lazy(() => import("./features/auth/pages/Login"));
+const Register = lazy(() => import("./features/auth/pages/Register"));
 
 function App() {
+  const { user, checkAuth, checkingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+
+  if(checkingAuth) {
+    return <Spinner />
+  }
+
   return (
-    <>
-      <BrowserRouter>
+    <BrowserRouter>
+      <Toaster richColors position="top-right" />
+      <Suspense fallback={<Spinner />}>
         <Routes>
-          <Route path="/" element={<Dashboard/>}></Route>
-          <Route path="/aset-perusahaan" element={<Dashboard/>}></Route>
-          <Route path="/aset-karyawan" element={<Dashboard/>}></Route>
-          <Route path="/kendaraan" element={<Dashboard/>}></Route>
-          <Route path="/ruangan" element={<Dashboard/>}></Route>
+          <Route path="/" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+          <Route path="/projects" element={user ? <Projects /> : <Navigate to="/login" />} />
+          <Route path="/peminjaman" element={user ? <LoansPage /> : <Navigate to="/login" />} />
+          <Route path="/pengembalian" element={user ? <ReturnsPage /> : <Navigate to="/login" />} />
+          <Route path="/aset-perusahaan" element={user ? <Assets /> : <Navigate to="/login" />} />
+          <Route path="/aset-karyawan" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+          <Route path="/kendaraan" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+          <Route path="/ruangan" element={user ? <Dashboard /> : <Navigate to="/login" />} />
+          <Route path="/login" element={!user ? <Login /> : <Navigate to="/" />} />
+          <Route path="/register" element={!user ? <Register /> : <Navigate to="/" />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
-      </BrowserRouter>
-    </>
+      </Suspense>
+    </BrowserRouter>
   );
 }
 
