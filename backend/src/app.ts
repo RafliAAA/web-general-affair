714bc091b1a5d3c5b92e154bac540b52a22ec93d
rafliAAA <raflialamsyah184@gmail.com>
2026-06-26 07:40:58 +0700

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,9 +4,14 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 
 import authRoutes from "./modules/auth/auth.routes";
+import userRoutes from "./modules/user/user.routes";
 import assetsRoutes from "./modules/assets/assets.routes";
+import procurementRoutes from "./modules/procurement/procurement.routes";
+import handoverRoutes from "./modules/handover/handover.routes";
 import borrowRoutes from "./modules/borrow/borrow.routes";
 import returnRoutes from "./modules/return/return.routes";
+import maintenanceRoutes from "./modules/maintenance/maintenance.routes";
+import disposalRoutes from "./modules/disposal/disposal.routes";
 
 dotenv.config();
 
@@ -22,8 +27,13 @@ app.use(
 );
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 app.use("/api/assets", assetsRoutes);
+app.use("/api/procurement", procurementRoutes);
+app.use("/api/handover", handoverRoutes);
 app.use("/api/borrow", borrowRoutes);
 app.use("/api/return", returnRoutes);
+app.use("/api/maintenance", maintenanceRoutes);
+app.use("/api/disposal", disposalRoutes);
 
 export default app;
