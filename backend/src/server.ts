0068812cddfc596cef4775281f8126b66474d82e
rafliAAA <raflialamsyah184@gmail.com>
2026-08-06 07:03:20 +0700

    update

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,11 @@
 import app from "./app"
+import { initSocket } from "./config/socket";
+import http from "http";
+
+const server = http.createServer(app);
+initSocket(server);
 
 const PORT = process.env.PORT
-
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);   
 })
