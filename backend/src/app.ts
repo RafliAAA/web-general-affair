d67e229c5d5689a1687fe62771967d2db2fe7331
rafliAAA <raflialamsyah184@gmail.com>
2026-08-06 07:02:48 +0700

    update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,6 +6,9 @@ import cors from "cors";
 import authRoutes from "./modules/auth/auth.routes";
 import dashboardRoutes from "./modules/dashboard/dashboard.routes"
 import userRoutes from "./modules/user/user.routes";
+import profileRoutes from "./modules/profile/profile.routes";
+import entityRoutes from "./modules/entity/entity.routes"
+import directorateRoutes from "./modules/directorate/directorate.routes"
 import assetsRoutes from "./modules/assets/assets.routes";
 import procurementRoutes from "./modules/procurement/procurement.routes";
 import handoverRoutes from "./modules/handover/handover.routes";
@@ -15,6 +18,7 @@ import maintenanceRoutes from "./modules/maintenance/maintenance.routes";
 import disposalRoutes from "./modules/disposal/disposal.routes";
 import roomRoutes from "./modules/rooms/rooms.routes"
 import bookingRoutes from "./modules/bookings/bookings.routes"
+import notificationRoutes from "./modules/notifications/notification.routes"
 
 dotenv.config();
 
@@ -32,6 +36,9 @@ app.use(
 app.use("/api/auth", authRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/users", userRoutes);
+app.use("/api/profile", profileRoutes);
+app.use("/api/directorate", directorateRoutes)
+app.use("/api/entity", entityRoutes)
 app.use("/api/assets", assetsRoutes);
 app.use("/api/procurement", procurementRoutes);
 app.use("/api/handover", handoverRoutes);
@@ -41,5 +48,6 @@ app.use("/api/maintenance", maintenanceRoutes);
 app.use("/api/disposal", disposalRoutes);
 app.use("/api/rooms", roomRoutes)
 app.use("/api/bookings", bookingRoutes)
+app.use("/api/notifications", notificationRoutes)
 
 export default app;
