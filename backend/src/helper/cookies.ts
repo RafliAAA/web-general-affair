4af30ea08b8f20da2f0852ec30281a9ae210f311
rafliAAA <raflialamsyah184@gmail.com>
2026-08-26 03:47:26 +0700

    chore(auth): extend access token expiration

--- backend/src/helper/cookies.ts
+++ backend/src/helper/cookies.ts
@@ -8,15 +8,15 @@ export const setCookies = (
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
-    maxAge: 15 * 60 * 1000,
+    sameSite: "lax",
+    maxAge: 60 * 60 * 1000,
     path: "/",
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "none",
+    sameSite: "lax",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
